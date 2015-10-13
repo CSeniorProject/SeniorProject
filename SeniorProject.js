16fe06973293fbbd39bac65eb092fa01864aab19
elifdogali <elif.dogali@ozu.edu.tr>
2015-10-13 22:03:06 +0300

    First commit

---
+++ SeniorProject.js
@@ -0,0 +1,14 @@
+if (Meteor.isClient) {
+	
+  Template.Game.onRendered(function () {
+	  $( "#draggable_1" ).draggable();
+	  $( "#draggable_2" ).draggable();
+	  $( "#draggable_3" ).draggable();
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
