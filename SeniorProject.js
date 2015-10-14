1bfa5d48f6e1fc7c95af132ed9e9b8afe1fa829a
ezgi,ozdil <student@STUDENT-PC>
2015-10-15 00:12:28 +0300

    js&css editing

--- SeniorProject.js
+++ SeniorProject.js
@@ -1,9 +1,17 @@
 if (Meteor.isClient) {
-
   Template.hello.onRendered(function () {
 	  $( "#draggable_1" ).draggable();
 	  $( "#draggable_2" ).draggable();
 	  $( "#draggable_3" ).draggable();
+	  $("#panel2").droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+	  
   });
 }
 
