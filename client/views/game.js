23a410d4f03c0fef7b3250c3b1dc490842a9f101
elifdogali <elif.dogali@ozu.edu.tr>
2015-11-22 18:28:53 +0200

    Alert added

--- client/views/game.js
+++ client/views/game.js
@@ -1,4 +1,4 @@
-Template.game.helpers({
+ Template.game.helpers({
 
  codes: function () {
      return CodeLabels.find({}, {sort: {createdAt: -1}});
@@ -7,6 +7,8 @@ Template.game.helpers({
 
 Template.game.onRendered(function () {
 	  
+	  
+	  
 	  $( ".item" ).draggable({
 		cancel: "a.ui-icon", // clicking an icon won't initiate dragging
         revert: "invalid", // when not dropped, the item will revert back to its initial position
@@ -14,7 +16,12 @@ Template.game.onRendered(function () {
         helper: "clone", // create "copy" with original properties, but not a true clone
         cursor: "move",
         revertDuration: 0, // immediate snap
-		connectToSortable: '.droppable-part'
+		connectToSortable: '.droppable-part',
+		
+		start: function(event, ui) {
+            sAlert.info("You clicked button");
+        }
+        
 		
 		/*stop: function(event,ui) { 
 			$('.droppable-part>a').replaceWith('<div>' + $(this).text() + '</div>'); 
@@ -23,7 +30,26 @@ Template.game.onRendered(function () {
 	  });
 	  
 	  $(".droppable-part").sortable({
+	
+		  connectWith : ".droppable-part",
+		  
+		  change: function(event, ui) {
+            assert("change");
+        },
+        
+		update: function(event, ui) {
+		
+			var end_pos = ui.item.index();
+            var start_pos = ui.item.data('start_pos');
+			var item = {};
+			item.name = "Test 2";
+			item.start_pos =start_pos;
+			item.end_pos = end_pos;
+	    
+        }
+ 
 		//cancel: '#droppable-part>div'
+
 	  });
 	  
 	  $(".droppable-part").droppable({
@@ -32,11 +58,16 @@ Template.game.onRendered(function () {
 		activeClass: 'droppable-active',
         hoverClass: 'droppable-hover',
         activeClass: "ui-state-highlight",
+		
         drop: function( event, ui ) {
             // clone item to retain in original "list"
             var $item = ui.draggable.clone();
             $(this).addClass('has-drop').html($item);
+			($item).addClass('dropped-item');
 			
+			
+			//sAlert.info('dropped', configOverwrite);
+	
 			var text = $item.text;
 			
 			CodeLabels.insert({
@@ -48,6 +79,9 @@ Template.game.onRendered(function () {
         }
     });
 	
+	/*("#sortable").sortable({
+        
+    });*/
 });
 	
 Template.game.events({
@@ -72,7 +106,6 @@ Template.game.events({
      event.preventDefault();
      // Get value from form element
      var text = event.target.text.value;
-
      // Insert a task into the collection
      CodeLabels.insert({
        text: text,
