cd3aa334e50e12c9ed78e5cb16226f39c9f9e30c
sena.kaya <sena.kaya@ozu.edu.tr>
2015-11-23 20:54:58 +0200

    arraylist and delete button added

--- client/views/game.js
+++ client/views/game.js
@@ -1,121 +1,114 @@
- Template.game.helpers({
+var orderedMoves = function () {
+  return Moves.find({}, {
+    sort: {
+      index: 1
+    }
+  });
+};
 
- codes: function () {
-     return CodeLabels.find({}, {sort: {createdAt: -1}});
-   }
- });
-
-Template.game.onRendered(function () {
-	  
-	  
-	  
-	  $( ".item" ).draggable({
-		cancel: "a.ui-icon", // clicking an icon won't initiate dragging
-        revert: "invalid", // when not dropped, the item will revert back to its initial position
-        //revert: true, // bounce back when dropped
-        helper: "clone", // create "copy" with original properties, but not a true clone
-        cursor: "move",
-        revertDuration: 0, // immediate snap
-		connectToSortable: '.droppable-part',
-		
-		start: function(event, ui) {
-            sAlert.info("You clicked button");
-        }
-        
-		
-		/*stop: function(event,ui) { 
-			$('.droppable-part>a').replaceWith('<div>' + $(this).text() + '</div>'); 
-		}*/
-
-	  });
-	  
-	  $(".droppable-part").sortable({
-	
-		  connectWith : ".droppable-part",
-		  
-		  change: function(event, ui) {
-            assert("change");
-        },
-        
-		update: function(event, ui) {
-		
-			var end_pos = ui.item.index();
-            var start_pos = ui.item.data('start_pos');
-			var item = {};
-			item.name = "Test 2";
-			item.start_pos =start_pos;
-			item.end_pos = end_pos;
-	    
-        }
- 
-		//cancel: '#droppable-part>div'
-
-	  });
-	  
-	  $(".droppable-part").droppable({
-        accept: ".items .item",
-		greedy: true,
-		activeClass: 'droppable-active',
-        hoverClass: 'droppable-hover',
-        activeClass: "ui-state-highlight",
-		
-        drop: function( event, ui ) {
-            // clone item to retain in original "list"
-            var $item = ui.draggable.clone();
-            $(this).addClass('has-drop').html($item);
-			($item).addClass('dropped-item');
-			
-			
-			//sAlert.info('dropped', configOverwrite);
-	
-			var text = $item.text;
-			
-			CodeLabels.insert({
-				text: text,
-				createdAt: new Date() // current time
-			});
-			
-			console.log("inserted");
-        }
-    });
-	
-	/*("#sortable").sortable({
-        
-    });*/
+Template.allMoves.helpers({
+  moves: orderedMoves
 });
-	
-Template.game.events({
 
-	/*"click .item": fuction(event) {
-		event.preventDefault
-		var text = event.target.text.value;	
-	}*/
-	
-    /*this.$('.item').draggable({
-        cancel: "a.ui-icon", // clicking an icon won't initiate dragging
-        //revert: "invalid", // when not dropped, the item will revert back to its initial position
-        revert: true, // bounce back when dropped
-        helper: "clone", // create "copy" with original properties, but not a true clone
-        cursor: "move"
-        , revertDuration: 0 // immediate snap
-    });*/
-    
-   
-   /*"submit .new-task": function (event) { 
-     // Prevent default browser form submit
-     event.preventDefault();
-     // Get value from form element
-     var text = event.target.text.value;
-     // Insert a task into the collection
-     CodeLabels.insert({
-       text: text,
-       createdAt: new Date() // current time
-     });
-   }*/
- });
+Template.game.helpers({
+  moveTypes: function() {
+    return [{
+      _id: "move",
+      label: "MOVE"
+    }, {
+      _id: "turn-left",
+      label: "TURN LEFT"
+    }, {
+      _id: "turn-right",
+      label: "TURN RIGHT"
+    }];
+  },
 
- Template.Code.events({
-   "click .delete": function () {
-     CodeLabels.remove(this._id);
-   }
- });
+  moves: orderedMoves
+});
+
+Template.moveTypeItem.onRendered(function() {
+  this.$(".draggable-item").draggable({
+    cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+    revert: "invalid", // when not dropped, the item will revert back to its initial position
+    //revert: true, // bounce back when dropped
+    helper: "clone", // create "copy" with original properties, but not a true clone
+    cursor: "move",
+    revertDuration: 0, // immediate snap
+    connectToSortable: '.droppable-part'
+
+  });
+});
+
+Template.game.onRendered(function() {
+  var self = this;
+  var $sortable = self.$('.sortable');
+  $sortable.sortable({
+    connectWith: ".sortable",
+    update: function(event, ui) {
+      var itemsToUpdate = [];
+      $sortable.find('.move-item').each(function () {
+        itemsToUpdate.push({
+          _id: $(this).data('id'),
+          index: $(this).index()
+        });
+      });
+
+      Meteor.defer(function () {
+        _.each(itemsToUpdate, function (item) {
+          Moves.update(item._id, {
+            $set: {
+              index: item.index
+            }
+          });
+        });
+      });
+    }
+  });
+
+  this.$(".droppable").droppable({
+    accept: ".draggable-item",
+    activeClass: 'droppable-active',
+    hoverClass: 'droppable-hover',
+    activeClass: "ui-state-highlight",
+
+    drop: function (event, ui) {
+      var id = ui.draggable.data('id');
+      var label = ui.draggable.data('label');
+
+      var newMove = {
+        type: id,
+        label: label,
+        createdAt: new Date()
+      };
+
+      var latestItem = Moves.findOne({
+
+      }, {
+        sort: {
+          index: -1
+        }
+      });
+
+      if(latestItem) {
+        newMove.index = latestItem.index + 1;
+      } else {
+        newMove.index = 0;
+      }
+
+      newMove._id = Moves.insert(newMove);
+
+      console.log('added:', newMove);
+    }
+  });
+
+  // this.$('.droppable').on('drop', function () {
+  //   debugger
+  // });
+});
+
+Template.moveItem.events({
+  "click .delete": function() {
+    Moves.remove(this._id);
+  }
+});
